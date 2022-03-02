1041ac2bee9bbb3c7348051ceb3c6b0488c950df
Thiago Leite <leite.tjs@gmail.com>
2022-03-02 16:01:18 -0300

    feat: conditional loading working

--- src/components/loadfile/LoadFile.jsx
+++ src/components/loadfile/LoadFile.jsx
@@ -1,7 +1,11 @@
 import style from 'styled-components';
 
 const LoadFile = style.section`
-  background-color: black;
+  background-color: white;
+
+  ul {
+    background-color: white;
+  }
 `;
 
 export default LoadFile;
--- src/components/loadfile/index.jsx
+++ src/components/loadfile/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoadFile from './LoadFile';
 
 export default function index() {
+  const [fileInfo, setFileInfo] = useState([{ id: 1, name: 'thiago', email: 'test@example.com' }]);
+  console.log(setFileInfo);
   return (
     <LoadFile>
-      teste
+      {
+        !fileInfo
+          ? null
+          : (
+            <ul>
+              {fileInfo.map(({ id, name, email }) => (
+                <li>{`ID: ${id} Nome: ${name} Email: ${email}`}</li>
+              ))}
+            </ul>
+          )
+    }
     </LoadFile>
   );
 }
